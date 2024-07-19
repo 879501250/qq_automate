22dba4bd03c95541bdee01370a079747c35afd29
qiqin <qiqin@primeton.com>
2024-07-19 10:38:05 +0800

    add:qq_automate初始代码提交

---
+++ src/main/java/org/qq/automate/QqAutomateApplication.java
@@ -0,0 +1,13 @@
+package org.qq.automate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqAutomateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QqAutomateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/qq/automate/QqAutomateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qq.automate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QqAutomateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
